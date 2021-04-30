a36a4b7f5a92443c88f35f3a0e209a017c96bb75
siike92 <siike92@hotmail.com>
2021-04-29 19:07:06 -0500

    Added back in the spi test code

--- target/CM7/main.cpp
+++ target/CM7/main.cpp
@@ -1,6 +1,6 @@
 #include "../../lib/STM32h745zi-HAL/llpd/include/LLPD.hpp"
 
-// #include "SRAM_23K256.hpp"
+#include "SRAM_23K256.hpp"
 
 const int SYS_CLOCK_FREQUENCY = 480000000;
 
@@ -30,7 +30,7 @@ int main(void)
 	LLPD::usart_log( USART_NUM::USART_1, "Gen_MAX_FX_SYN starting up ----------------------------" );
 
 	// audio timer setup (for 40 kHz sampling rate at 480 MHz timer clock)
-	LLPD::tim6_counter_setup( 0, 12000, 40000 );
+	LLPD::tim6_counter_setup( 0, 6000, 40000 );
 	LLPD::tim6_counter_enable_interrupts();
 	LLPD::usart_log( USART_NUM::USART_1, "tim6 initialized..." );
 
@@ -39,17 +39,11 @@ int main(void)
 	LLPD::gpio_output_set( GPIO_PORT::A, GPIO_PIN::PIN_0, ledIsOn );
 
 	// spi initialization
-	/*
 	LLPD::spi_master_init( SPI_NUM::SPI_2, SPI_BAUD_RATE::SYSCLK_DIV_BY_256, SPI_CLK_POL::LOW_IDLE, SPI_CLK_PHASE::FIRST,
 				SPI_DUPLEX::FULL, SPI_FRAME_FORMAT::MSB_FIRST, SPI_DATA_SIZE::BITS_8 );
-				*/
-
-	// audio timer start
-	LLPD::tim6_counter_start();
-	LLPD::usart_log( USART_NUM::USART_1, "tim6 started..." );
+	LLPD::usart_log( USART_NUM::USART_3, "sram initialized..." );
 
 	// SRAM setup and test
-	/*
 	LLPD::gpio_output_setup( GPIO_PORT::B, GPIO_PIN::PIN_12, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL,
 				GPIO_OUTPUT_SPEED::HIGH ); // SRAM chip select pin
 	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, true );
@@ -57,18 +51,24 @@ int main(void)
 	SharedData<uint8_t> sramValsToWrite = SharedData<uint8_t>::MakeSharedData( 3 );
 	sramValsToWrite[0] = 25; sramValsToWrite[1] = 16; sramValsToWrite[2] = 8;
 	sram.writeToMedia( sramValsToWrite, 45 );
-	SharedData<uint8_t> retVals2 = sram.readFromMedia( 3, 45 );
-	if ( retVals2[0] == 25 && retVals2[1] == 16 && retVals2[2] == 8 )
+	SharedData<uint8_t> retVals = sram.readFromMedia( 3, 45 );
+	if ( retVals[0] == 25 && retVals[1] == 16 && retVals[2] == 8 )
 	{
-		// LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 0: ", retVals2[0] );
-		// LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 1: ", retVals2[1] );
-		// LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 2: ", retVals2[2] );
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 0: ", retVals[0] );
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 1: ", retVals[1] );
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 2: ", retVals[2] );
 	}
 	else
 	{
-		keepBlinking = false;
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 0: ", retVals[0] );
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 1: ", retVals[1] );
+		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 2: ", retVals[2] );
 	}
-	*/
+
+
+	// audio timer start
+	LLPD::tim6_counter_start();
+	LLPD::usart_log( USART_NUM::USART_1, "tim6 started..." );
 
 	while ( true )
 	{
