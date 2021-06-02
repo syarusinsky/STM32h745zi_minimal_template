957b979572afcf8a37debb25071f8c44fcd5fab0
siike92 <siike92@hotmail.com>
2021-06-02 14:16:56 -0500

    Modified test code to test sram and eeprom

--- target/CM7/main.cpp
+++ target/CM7/main.cpp
@@ -35,39 +35,65 @@ int main(void)
 	LLPD::usart_log( USART_NUM::USART_1, "tim6 initialized..." );
 
 	// test led setup
-	LLPD::gpio_output_setup( GPIO_PORT::A, GPIO_PIN::PIN_0, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::LOW );
-	LLPD::gpio_output_set( GPIO_PORT::A, GPIO_PIN::PIN_0, ledIsOn );
+	LLPD::gpio_output_setup( GPIO_PORT::A, GPIO_PIN::PIN_1, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL, GPIO_OUTPUT_SPEED::LOW );
+	LLPD::gpio_output_set( GPIO_PORT::A, GPIO_PIN::PIN_1, ledIsOn );
 
 	// spi initialization
 	LLPD::spi_master_init( SPI_NUM::SPI_2, SPI_BAUD_RATE::SYSCLK_DIV_BY_256, SPI_CLK_POL::LOW_IDLE, SPI_CLK_PHASE::FIRST,
 				SPI_DUPLEX::FULL, SPI_FRAME_FORMAT::MSB_FIRST, SPI_DATA_SIZE::BITS_8 );
 	LLPD::usart_log( USART_NUM::USART_3, "sram initialized..." );
 
-	// SRAM setup and test
-	LLPD::gpio_output_setup( GPIO_PORT::B, GPIO_PIN::PIN_12, GPIO_PUPD::NONE, GPIO_OUTPUT_TYPE::PUSH_PULL,
+	// i2c initialization
+	LLPD::i2c_master_setup( I2C_NUM::I2C_1, 0x308075AE );
+	LLPD::usart_log( USART_NUM::USART_3, "i2c initialized..." );
+
+	// sram cs setup
+	LLPD::gpio_output_setup( GPIO_PORT::B, GPIO_PIN::PIN_12, GPIO_PUPD::PULL_UP, GPIO_OUTPUT_TYPE::PUSH_PULL,
 				GPIO_OUTPUT_SPEED::HIGH ); // SRAM chip select pin
 	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, true );
-	Sram_23K256 sram( SPI_NUM::SPI_2, GPIO_PORT::B, GPIO_PIN::PIN_12 );
-	SharedData<uint8_t> sramValsToWrite = SharedData<uint8_t>::MakeSharedData( 3 );
-	sramValsToWrite[0] = 25; sramValsToWrite[1] = 16; sramValsToWrite[2] = 8;
-	sram.writeToMedia( sramValsToWrite, 45 );
-	SharedData<uint8_t> retVals = sram.readFromMedia( 3, 45 );
-	if ( retVals[0] == 25 && retVals[1] == 16 && retVals[2] == 8 )
-	{
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 0: ", retVals[0] );
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 1: ", retVals[1] );
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM worked! value 2: ", retVals[2] );
-	}
-	else
-	{
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 0: ", retVals[0] );
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 1: ", retVals[1] );
-		LLPD::usart_log_int( USART_NUM::USART_3, "SRAM failed! value 2: ", retVals[2] );
-	}
 
 	// audio timer start
 	LLPD::tim6_counter_start();
 	LLPD::usart_log( USART_NUM::USART_1, "tim6 started..." );
+
+	// quick spi test
+	LLPD::tim6_delay( 10000 ); // TODO arbitrary delay since if we immediately use the sram it can be wrong
+	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, false );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000010 );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000000 );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000000 );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 45 );
+	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, true );
+
+	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, false );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000011 );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000000 );
+	LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000000 );
+	uint8_t spiRetVal = LLPD::spi_master_send_and_recieve( SPI_NUM::SPI_2, 0b00000000 );
+	LLPD::gpio_output_set( GPIO_PORT::B, GPIO_PIN::PIN_12, true );
+	if ( spiRetVal == 45 )
+	{
+		LLPD::usart_log( USART_NUM::USART_1, "sram verified..." );
+	}
+	else
+	{
+		LLPD::usart_log( USART_NUM::USART_1, "unable to verify sram..." );
+	}
+
+	// quick i2c test
+	LLPD::i2c_master_set_slave_address( I2C_NUM::I2C_1, I2C_ADDR_MODE::BITS_7, 0b1010000 );
+	LLPD::i2c_master_write( I2C_NUM::I2C_1, true, 3, 0, 0, 24 );
+	LLPD::i2c_master_write( I2C_NUM::I2C_1, false, 2, 0, 0 );
+	uint8_t i2cRetVal = 0;
+	LLPD::i2c_master_read( I2C_NUM::I2C_1, true, 1, &i2cRetVal );
+	if ( i2cRetVal == 24 )
+	{
+		LLPD::usart_log( USART_NUM::USART_1, "eeprom verified..." );
+	}
+	else
+	{
+		LLPD::usart_log( USART_NUM::USART_1, "unable to verify eeprom..." );
+	}
 
 	while ( true )
 	{
@@ -87,7 +113,7 @@ extern "C" void TIM6_DAC_IRQHandler (void)
 			else
 			{
 				ledIsOn = !ledIsOn;
-				LLPD::gpio_output_set( GPIO_PORT::A, GPIO_PIN::PIN_0, ledIsOn );
+				LLPD::gpio_output_set( GPIO_PORT::A, GPIO_PIN::PIN_1, ledIsOn );
 
 				ledIncr = 0;
 			}
